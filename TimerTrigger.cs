d83c41fbf5124cc161dafe459de42e10fe0c70aa
Snorre Søvold <snorresovold@gmail.com>
2023-09-05 14:10:58 +0200

    timer works

---
+++ TimerTrigger.cs
@@ -0,0 +1,38 @@
+using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function
+{
+    public class TimerTrigger
+    {
+        private readonly ILogger _logger;
+
+        public TimerTrigger(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<TimerTrigger>();
+        }
+
+        [Function("TimerTrigger")]
+        public void Run([TimerTrigger("*/10 * * * * *")] MyInfo myTimer)
+        {
+            _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+    }
+
+    public class MyInfo
+    {
+        public MyScheduleStatus ScheduleStatus { get; set; }
+
+        public bool IsPastDue { get; set; }
+    }
+
+    public class MyScheduleStatus
+    {
+        public DateTime Last { get; set; }
+
+        public DateTime Next { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+    }
+}
